52346c1c5bee84c137da5af1e5409828b6d8ef83
Jihoon Kim (Hanul) <jihoonkimtech@naver.com>
2025-11-17 14:16:19 +0900

    Update: run analyzer

--- short-form-EEG/analyze/compare_csv.py
+++ short-form-EEG/analyze/compare_csv.py
@@ -119,7 +119,7 @@ def create_comparison_plot():
     plt.xlabel('testcase', fontsize=12)
     
     # set fixed Y-axis (slightly taller for labels)
-    plt.ylim(-0.5, 2.5)
+    plt.ylim(-0.5, 11.5)
     
     # rotate x-axis labels to be horizontal
     plt.xticks(rotation=0, fontsize=11)
